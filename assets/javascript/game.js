b0d29ce8a6864387306abcb21e4a72d505f15362
tonyscott44 <acsii@me.com>
2020-10-01 03:39:05 -0400

    solving for mobile responsiveness

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,12 +42,12 @@ $(document).ready(function() {
     if (event.keyCode == 32 || event.keyCode == 13) { // If spacebar or enter is pressed,  
         userGuess = "_";                              // default the user guess to "_", due to
     }                                                 // spacebar event data corrupting game/events
-    console.log("47");
+    console.log("44");
     console.log(event);
     if(keyStroke === 1){  // If user's first try...
         document.getElementById("gbTitle").innerHTML = "";
         document.getElementById("gbSubTitle").innerHTML = "";
-        document.getElementById("gbSubTitle2").innerHTML = "";
+        // document.getElementById("gbSubTitle2").innerHTML = "";
         document.getElementById("gameMsg").innerHTML = "";
         document.getElementById("gameMsg2").innerHTML = "";
 
